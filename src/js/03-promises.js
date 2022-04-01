e21337cc68ec82e44a62eff02951a26f3fc8a4b6
Leevorum <di4111ua@gmail.com>
2022-04-01 17:19:49 +0300

    add disabler for create btn

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,5 +1,6 @@
 import Notiflix from 'notiflix';
 // Выбираем форму
+const createPromiseBtn = document.querySelector('button');
 const formElem = document.querySelector('.form');
 // Функция создания промиса
 function createPromise(position, delay) {
@@ -14,6 +15,18 @@ function createPromise(position, delay) {
     }, delay);
   });
 }
+//Функция для разблокировки кнопки после выполнения всех промисов
+function createBtnDisabler(delayPromise) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (delayPromise) {
+        resolve((createPromiseBtn.disabled = false));
+      } else {
+        reject((createPromiseBtn.disabled = true));
+      }
+    }, delayPromise);
+  });
+}
 
 // Функция сабмита по форме
 const handleFormSubmit = e => {
@@ -25,6 +38,10 @@ const handleFormSubmit = e => {
   const amountPromise = Number(amount.value);
   // Циклом создаем промисы
   for (let i = 1; i <= amountPromise; i += 1) {
+    //Блокируем кнопку
+    if (i < amountPromise) {
+      createPromiseBtn.disabled = true;
+    }
     createPromise(i, delayPromise)
       .then(({ position, delay }) => {
         Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
@@ -35,6 +52,9 @@ const handleFormSubmit = e => {
     //Суммируем задержку для следующего промиса
     delayPromise += stepPromise;
   }
+  console.log(delayPromise);
+  //Вызываем функцию разблокирования кнопки после цикла
+  createBtnDisabler(delayPromise);
 };
 // Слушатель на форме по сабмиту
 formElem.addEventListener('submit', handleFormSubmit);
